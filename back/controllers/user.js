c54b773f0b20cda277770584814c783c735d5bf4
Romain <hercez92@hotmail.fr>
2022-01-18 19:23:09 +0100

    replace placeholder routes with actual signup and login

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,12 +1,44 @@
+const UsersHandlingService = require('../domain/UsersHandlingService');
+const User = require('../domain/User');
+const InMemoryUserRepository = require('../domain/mock/InMemoryUserRepository');
+
+class UserRequestDTO {
+    constructor({email, password}) {
+        this.email = email,
+        this.password = password
+    }
+}
+
+const placeholderUser = new User(
+    '123@123.fr',
+    "123"
+);
+placeholderUser.userId = 'placeholder userId'
+
+const userRepository = new InMemoryUserRepository([placeholderUser]);
+const usersHandlingService = new UsersHandlingService(userRepository);
+
 exports.signup = (req, res, next) => {
-    res.status(201).json({
-        message: 'New user created !'
+    const userSignupRequest = req.body;
+    const newUserRequestDTO = new UserRequestDTO({
+        ...userSignupRequest
     });
+    try {
+        usersHandlingService.createUser(newUserRequestDTO);
+        res.status(201).json({
+            message: 'New user created !'
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 exports.login = (req, res, next) => {
-    res.status(200).json({
-        userId: 'free_login_for_everyone',
-        token: 'super_secret_token'
-    });
+    try {
+        const userLoginRequest = req.body;
+        const userLogin = usersHandlingService.createLoginSession(userLoginRequest);
+        res.status(200).json(userLogin);    
+    } catch (error) {
+        res.status(401).json(error);
+    }
 }
