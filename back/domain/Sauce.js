7d5752b41eede2efe6083f5377511004899ea439
Romain <hercez92@hotmail.fr>
2022-01-18 19:19:10 +0100

    add id generation with uuid

--- back/domain/Sauce.js
+++ back/domain/Sauce.js
@@ -1,37 +1,19 @@
-// class Sauce {
-//     constructor({_id, userId, name, manufacturer, description, mainPepper, imageUrl, heat, likes, dislikes, usersLiked, usersDisliked}) {
-//         this._id = _id;
-//         this.userId = userId,
-//         this.name = name,
-//         this.manufacturer = manufacturer,
-//         this.description = description,
-//         this.mainPepper = mainPepper,
-//         this.imageUrl = imageUrl,
-//         this.heat = heat,
-//         this.likes = likes,
-//         this.dislikes = dislikes,
-//         this.usersLiked = usersLiked,
-//         this.usersDisliked = usersDisliked
-//     }
-// }
+const { v4: uuidv4 } = require('uuid');
 
 class Sauce {
-    constructor(_id, name, description, manufacturer, mainPepper, heat, imageFileName, userId) {
-        this._id = _id,
-        this.name = name,
-        this.description = description,
-        this.manufacturer = manufacturer,
-        this.mainPepper = mainPepper,
-        this.heat = heat,
-        this.userId = userId,
-        this.imageUrl = `http://localhost:3000/images/${imageFileName}`,
-        this.likes = 0,
-        this.dislikes = 0,
-        this.userLiked = [],
-        this.userDisliked = []
-        if (!_id) {
-            this._id = `${userId}-${name}`
-        }
+    constructor({id = uuidv4(), userId, name, description, manufacturer, mainPepper, heat, imageFileName, likes = 0, dislikes = 0, userLiked = [], userDisliked = []}) {
+        this._id = id;
+        this.userId = userId;
+        this.name = name;
+        this.description = description;
+        this.manufacturer = manufacturer;
+        this.mainPepper = mainPepper;
+        this.heat = heat;
+        this.imageUrl = `http://localhost:3000/images/${imageFileName}`;
+        this.likes = likes;
+        this.dislikes = dislikes;
+        this.userLiked = userLiked;
+        this.userDisliked = userDisliked;
     }
 }
 
