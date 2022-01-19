0727ad6a970e5a3347bba7e145c271287b86cfb0
Romain <hercez92@hotmail.fr>
2022-01-19 03:19:59 +0100

    test password generation and verification

--- back/domain/UsersHandlingService.spec.js
+++ back/domain/UsersHandlingService.spec.js
@@ -3,6 +3,8 @@ const User = require('./User');
 const UserPassword = require('./UserPassword');
 const InMemoryUserRepository = require('./mock/InMemoryUserRepository');
 const TokenService = require('./TokenService')
+const bcrypt = require('bcrypt');
+
 
 describe('UsersHandlingService', () => {
     it('Should return a specific user from a list of users', () => {
@@ -13,27 +15,44 @@ describe('UsersHandlingService', () => {
         expect(usersHandlingService.getOneUser('email')).toEqual(expectedUser);
     })
     it('Should create a new user', () => {
-        const newUser = new User({email: 'email'})
+        const newUser = new User({email: 'email', password: 'password'})
         const userRepository = new InMemoryUserRepository([]);
         const usersHandlingService = new UsersHandlingService(userRepository);
         usersHandlingService.createUser(newUser);
         expect(usersHandlingService.getOneUser('email')).toBeDefined();
     })
     it('Should create a userId', () => {
-        const newUser = new User({email: 'email'})
+        const newUser = new User({email: 'email', password: 'password'})
         const userRepository = new InMemoryUserRepository([]);
         const usersHandlingService = new UsersHandlingService(userRepository);
         usersHandlingService.createUser(newUser);
         expect(usersHandlingService.getOneUser('email').userId).toBeDefined();
     })
-    it('Should return an object containing a user Id and a token', () => {
-        const user = new User({email: 'email'});
-        const userRepository = new InMemoryUserRepository([user]);
+    it('Should return an object containing a user Id and a token', async () => {
+        const userData = new User({email: 'email', password: new UserPassword('password').password});
+        const userRepository = new InMemoryUserRepository([userData]);
         const usersHandlingService = new UsersHandlingService(userRepository);
         const expectedLoginInfo = {
-            userId: user.userId,
-            token: TokenService.createUserToken(user.userId)
+            userId: userData.userId,
+            token: TokenService.createUserToken(userData.userId)
         }
-        expect(usersHandlingService.createLoginSession(user)).toEqual(expectedLoginInfo);
+        const user = new User({email: 'email', password: 'password'});
+        const loginSession = await usersHandlingService.createLoginSession(user)
+        expect(loginSession).toEqual(expectedLoginInfo);
+    })
+    it('Should have a password', () => {
+        const user = new User({email: 'email', password: 'password'});
+        const userRepository = new InMemoryUserRepository([]);
+        const usersHandlingService = new UsersHandlingService(userRepository);
+        usersHandlingService.createUser(user);
+        expect(usersHandlingService.getOneUser('email').password).toBeDefined();
+    })
+    it('Should compare password and hash and return true', () => {
+        const user = new User({email: 'email', password: 'testpassword'});
+        const userRepository = new InMemoryUserRepository([]);
+        const usersHandlingService = new UsersHandlingService(userRepository);
+        usersHandlingService.createUser(user);
+        const passwordHash = usersHandlingService.getOneUser('email').password;
+        expect(usersHandlingService.verifyPassword('testpassword', passwordHash)).toBeTruthy();
     })
 })
