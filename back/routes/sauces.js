6d5f2ac25894aad52adf62b94cd396bb7c77bb80
Romain <hercez92@hotmail.fr>
2022-01-29 00:10:43 +0100

    change getOneSauce to getSauceById

--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -6,7 +6,7 @@ const multer = require('../middleware/multer');
 const saucesController = require('../controllers/sauces');
 
 router.get('/', auth, saucesController.getAllSauces);
-router.get('/:id', auth, saucesController.getOneSauce);
+router.get('/:id', auth, saucesController.getSauceById);
 router.post('/', auth, multer, saucesController.postSauce);
 router.put('/:id', auth, multer, saucesController.updateSauce);
 router.delete('/:id', auth, saucesController.deleteSauce);
