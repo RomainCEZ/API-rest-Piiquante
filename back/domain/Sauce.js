12f5166c4b07eef5cc318ce727a9a2c12773e8ce
Romain <hercez92@hotmail.fr>
2022-01-28 23:03:26 +0100

    add like, dislike and cancel methods

--- back/domain/Sauce.js
+++ back/domain/Sauce.js
@@ -1,8 +1,8 @@
 const { v4: uuidv4 } = require('uuid');
 
 class Sauce {
-    constructor({id = uuidv4(), userId, name, description, manufacturer, mainPepper, heat, imageFileName, likes = 0, dislikes = 0, usersLiked = [], usersDisliked = []}) {
-        this._id = id;
+    constructor({id = uuidv4(), userId, name, description, manufacturer, mainPepper, heat, imageFileName, usersLiked = [], usersDisliked = []}) {
+        this.id = id;
         this.userId = userId;
         this.name = name;
         this.description = description;
@@ -10,10 +10,30 @@ class Sauce {
         this.mainPepper = mainPepper;
         this.heat = heat;
         this.imageUrl = `http://localhost:3000/images/${imageFileName}`;
-        this.likes = likes;
-        this.dislikes = dislikes;
         this.usersLiked = usersLiked;
         this.usersDisliked = usersDisliked;
+    }
+    get likes() {
+        return this.usersLiked.length;
+    }
+    get dislikes() {
+        return this.usersDisliked.length;
+    }
+    like(userId) {
+        if (!this.usersLiked.includes(userId)) {
+            this.usersLiked.push(userId);
+        }
+        this.usersDisliked = this.usersDisliked.filter( usersDisliked => usersDisliked !== userId);
+    }
+    dislike(userId) {
+        if (!this.usersDisliked.includes(userId)) {
+            this.usersDisliked.push(userId);
+        }
+        this.usersLiked = this.usersLiked.filter( usersLiked => usersLiked !== userId);
+    }
+    cancel(userId) {
+        this.usersLiked = this.usersLiked.filter( usersLiked => usersLiked !== userId);
+        this.usersDisliked = this.usersDisliked.filter( usersDisliked => usersDisliked !== userId);
     }
 }
 
